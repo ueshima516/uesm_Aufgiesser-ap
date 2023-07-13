f432c842131e6e3011d2077b9d6da2dc342a4230
k-mrnk <kentmuranaka@gmail.com>
2023-07-13 17:09:47 +0900

    リクエストから変数を読み込めるようにした

--- backend/schedule_lambda_withWrite.py
+++ backend/schedule_lambda_withWrite.py
@@ -10,22 +10,44 @@ target_dynamodb = boto3.resource('dynamodb').Table("yuasa-translate-history")
 EASY, NORMAL, HARD = [1,0,0,0,1,0,0], [1,0,1,0,1,0,1], [1,1,1,0,1,1,1]
 dic = {'EASY': EASY, 'NORMAL': NORMAL, 'HARD': HARD}
 
+
+start_date, end_date = None, None #"2023-00-00"
+start_time, end_time = None, None #"00:00"
+menu, mode = None, None #"running", "EASY or NORMAL or HARD"
+
+
 def lambda_handler(event, context):
     ### --- ここの固定値の指定をeventからの変数で渡したい --- ###
-    start_date, end_date = datetime.date(2023,8,1), datetime.date(2023,8,31)
-    start_time, end_time, work_time, menu, mode = "08:00", "08:30", "30", "running", "EASY"
-    ### --- ここまで --- ###
+    # start_date, end_date = datetime.date(2023,8,1), datetime.date(2023,8,31)
+    # start_time, end_time, work_time, menu, mode = "08:00", "08:30", "30", "running", "EASY"
 
-    output_all_schedule = make_schedule(start_date, end_date, start_time, end_time, work_time, menu, mode)
-    print(output_all_schedule)
+    body = json.loads(event["body"])
     
-    write_DynamoDB(output_all_schedule)
+    start_date = body["start_date"]
+    end_date = body["end_date"]
+    start_time = body["start_time"]
+    end_time = body["end_time"]
+    work_time = body["work_time"]
+    menu = body["menu"]
+    mode = body["mode"]
+
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
+    
+    ### --- ここまで --- ###
+    
+    output_all_schedule = make_schedule(start_date, end_date, start_time, end_time, work_time, menu, mode)
+
+    print(output_all_schedule)
+    # write_DynamoDB(output_all_schedule)
     
     return {
-        'statusCode': 200,
-        "body": json.dumps({"output_text": output_all_schedule}),
-        "isBase64Encoded": False,
-        "headers": {}
+      'statusCode': 200,
+      "body": json.dumps({"output_text": output_all_schedule}),
+      "isBase64Encoded": False,
+      "headers": {
+        "Access-Control-Allow-Origin": "*"
+      }
     }
 
 def  make_schedule(start_date, end_date, start_time, end_time, work_time, menu, mode):
