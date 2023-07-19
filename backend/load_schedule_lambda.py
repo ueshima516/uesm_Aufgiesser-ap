f238a96ec4b3b076974d24ef2f3a7765c13d270d
yuasa <y.kyuri2014@gmail.com>
2023-07-19 10:37:38 +0900

    DBから全要素スキャン

--- backend/load_schedule_lambda.py
+++ backend/load_schedule_lambda.py
@@ -1,7 +1,17 @@
 import json
+import boto3
 
 # 後々 data は DBから読み込めるようにする
 data = {"output_text": {"b25f387c-2155-11ee-b258-825e158fd254": {"date": "2023/08/01", "start_time": "10:00", "end_time": "11:30", "work_time": "30", "menu": "running"}, "b25f3ac0-2155-11ee-b258-825e158fd254": {"date": "2023/08/05", "start_time": "10:00", "end_time": "11:30", "work_time": "30", "menu": "running"}, "b25f3bba-2155-11ee-b258-825e158fd254": {"date": "2023/08/08", "start_time": "10:00", "end_time": "11:30", "work_time": "30", "menu": "running"}, "b25f3c96-2155-11ee-b258-825e158fd254": {"date": "2023/08/12", "start_time": "10:00", "end_time": "11:30", "work_time": "30", "menu": "running"}, "b25f3d5e-2155-11ee-b258-825e158fd254": {"date": "2023/08/15", "start_time": "10:00", "end_time": "11:30", "work_time": "30", "menu": "running"}}}
+TABLE_NAME = "fitshow_dev"
+
+
+def scan_db():
+    dynamodb = boto3.resource('dynamodb').Table(TABLE_NAME)
+    response = dynamodb.scan()
+
+    return response["Items"]
+
 
 def search_element_by_date(data, date_value):
     dic = {}
@@ -10,10 +20,13 @@ def search_element_by_date(data, date_value):
           dic.update({key: inner_dict})
     return dic
 
+
 def lambda_handler(event, context):
-    # DATEで日付を指定して要素を検索する
+    """ DATEで日付を指定して要素を検索する 
     DATE_TO_SEARCH = "2023/08/01"
     result = search_element_by_date(data["output_text"], DATE_TO_SEARCH)
+    """
+    result = scan_db()
     
     # 検索結果を出力する
     if result != {}:
