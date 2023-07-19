19a941b1890ebc51b599b6d81e72fc70ab378e84
yuasa <y.kyuri2014@gmail.com>
2023-07-19 12:02:29 +0900

    DB入力の仕様をDB構成に対応

--- backend/schedule_lambda_withWrite.py
+++ backend/schedule_lambda_withWrite.py
@@ -5,7 +5,7 @@ import json
 import boto3
 
 ### DB要変更
-target_dynamodb = boto3.resource('dynamodb').Table("yuasa-translate-history")
+target_dynamodb = boto3.resource('dynamodb').Table("fitshow_dev")
 
 EASY, NORMAL, HARD = [1,0,0,0,1,0,0], [1,0,1,0,1,0,1], [1,1,1,0,1,1,1]
 dic = {'EASY': EASY, 'NORMAL': NORMAL, 'HARD': HARD}
@@ -14,32 +14,37 @@ dic = {'EASY': EASY, 'NORMAL': NORMAL, 'HARD': HARD}
 start_date, end_date = None, None #"2023-00-00"
 start_time, end_time = None, None #"00:00"
 menu, mode = None, None #"running", "EASY or NORMAL or HARD"
+mail_address = None
+is_done = None # タスクが完了したか？
 
-
+DEBUG = False # eventの引数がないときはTrueにして動作させる
 def lambda_handler(event, context):
+
     ### --- ここの固定値の指定をeventからの変数で渡したい --- ###
-    # start_date, end_date = datetime.date(2023,8,1), datetime.date(2023,8,31)
-    # start_time, end_time, work_time, menu, mode = "08:00", "08:30", "30", "running", "EASY"
+    if(DEBUG==False):
+      body = json.loads(event["body"])
+      
+      start_date = body["start_date"]
+      end_date = body["end_date"]
+      start_time = body["start_time"]
+      end_time = body["end_time"]
+      work_time = body["work_time"]
+      menu = body["menu"]
+      mode = body["mode"]
+      mail_address = body["mail_address"] ## mail_address取得
+      start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
+      end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
 
-    body = json.loads(event["body"])
+    else:
+      start_date, end_date = datetime.date(2023,8,1), datetime.date(2023,8,31)
+      start_time, end_time, work_time, menu, mode = "08:00", "08:30", "30", "running", "EASY"
     
-    start_date = body["start_date"]
-    end_date = body["end_date"]
-    start_time = body["start_time"]
-    end_time = body["end_time"]
-    work_time = body["work_time"]
-    menu = body["menu"]
-    mode = body["mode"]
-
-    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d').date()
-    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d').date()
-    
+      mail_address = "supp0rt@c_egg.com" # メアドがフロントから欲しい
     ### --- ここまで --- ###
     
     output_all_schedule = make_schedule(start_date, end_date, start_time, end_time, work_time, menu, mode)
 
-    print(output_all_schedule)
-    # write_DynamoDB(output_all_schedule)
+    write_DynamoDB(mail_address, output_all_schedule)
     
     return {
       'statusCode': 200,
@@ -58,29 +63,29 @@ def  make_schedule(start_date, end_date, start_time, end_time, work_time, menu, 
   for idx in range(delta+1):
     flag = schedule_template[idx % 7] # テンプレート用の添字
     tar_date =  start_date + datetime.timedelta(days=idx)
-  
-    
-    if(flag==1):
-      res_ = {"date": tar_date.strftime('%Y/%m/%d'), "start_time": start_time, "end_time": end_time, "work_time": work_time, "menu": menu}
+
+
+    if(flag==1): # テンプレートに一致
+      # もし一日に複数の予定が入るなら引数のmenuをリストで渡す. リストの要素数に対してfor文回して計画を生成-> schedule{uuid-1: {予定1}, uuid-2: {予定2}}みたいな感じ
       uuid_ = str(uuid.uuid1())
-      res[uuid_] = res_
+      schedule = {uuid_: {"start_time": start_time, "end_time": end_time, "work_time": work_time, "menu": menu, "is_done": False}}
+      ### 
+
+      res[tar_date.strftime('%Y/%m/%d')] = schedule
+
   return res
 
 
 ### テスト段階でタイムスタンプとuuid, スケジュールを書き込み
-def write_DynamoDB(schedule_dic):
-  
-  for key_, value_ in schedule_dic.items():
-    # print(key_, uuid_)
-    timestamp = str(datetime.datetime.now())
-    # print(timestamp)
-    target_dynamodb.put_item(
-      Item={
-            'timestamp': timestamp,
-            'input_text': key_,
-            'output_text': value_
-        }
-    )
+def write_DynamoDB(mail_address, schedule_dic):
+
+  schedule_id = str(uuid.uuid1())
+  schedule_res = {"mail_address": mail_address, "schedule_id": schedule_id, schedule_id: schedule_dic}
+  print(schedule_res)
+
+  target_dynamodb.put_item(
+    Item=schedule_res
+  )
 
 
 
