b952de3cb3b7a2a94fc56a375a006c3ca1f9197c
zmr <this-is-dummy>
2023-07-24 16:40:33 +0900

    add: カレンダー日付欄に文字･アイコン表示、クリックでポップアップ
    - 要yarn install

--- frontend/my-app/components/Calendar/Calendar.jsx
+++ frontend/my-app/components/Calendar/Calendar.jsx
@@ -2,11 +2,35 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
+import Image from 'next/image';
+import Modal from 'react-modal';
+
+import scheduleIcon from '@/public/images/running-shoe.png';
+
 function MyCalendar() {
   const [date, setDate] = useState(new Date());
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const days = ["2023-07-19", "2023-07-23", "2023-07-25"]; // ここはDBから取得したデータを使う
 
   const handleDateChange = (value) => {
     setDate(value);
+  };
+
+  const tileContent = ({ date }) => {
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit', timeZone: 'Asia/Tokyo' };
+    const formattedDate = date.toLocaleDateString('ja-JP', options).replace(/\//g, '-');
+    return days.includes(formattedDate) ?
+      <div>予定あり <Image src={scheduleIcon} width={15} height={15} alt="予定あり" />  </div>
+      : null;
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
@@ -16,9 +40,19 @@ function MyCalendar() {
         <Calendar
           value={date}
           onChange={handleDateChange}
-          locale="ja-JP" //これを明示的に書かないと、/calenderページでブラウザリロードでエラーが出る
+          locale="ja-JP"
+          tileContent={tileContent}
+          onClickDay={openModal} // 追加
         />
       </div>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="ポップアップウィンドウ"
+      >
+        <h3>Hello!</h3>
+        <button onClick={closeModal}>閉じる</button> {/* 追加 */}
+      </Modal>
     </div>
   );
 }
