aad38fd05c0260460b3afa7572a172567dc1f77c
TsutsuiShinsuke <tsutsuics21@gmail.com>
2023-07-31 10:39:49 +0900

    分析画面の完成

--- frontend/my-app/pages/analysis.jsx
+++ frontend/my-app/pages/analysis.jsx
@@ -1,16 +1,13 @@
 "use client";
 
-import React, { useEffect, useState }  from "react";
+import React from "react";
 import { useAuth } from "@/components/Cognito/UseAuth";
 import Navigation from "@/components/Navigation/Navigation"
 import WeeklyAchievement from "@/components/Analysis/WeeklyAchievement"
-import Home from "@/components/Home/Home";
 import MonthlyAcheivement from "@/components/Analysis/MonthlyAchievement";
 import PrivateRoute from "@/components/Cognito/PrivateRoute";
 
 
-import MonthlyRank from "@/components/Analysis/MonthlyRank";
-import MonthlyCallAPI from "@/components/Analysis/MonthlyCallAPI";
 
 export default function HomePage () {
   const { isLoading } = useAuth();
