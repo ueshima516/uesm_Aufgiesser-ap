1d9ff0f629a3ac5a2c431380f60cff4517cca7ae
Yuki Matsuyoshi <yu-matsuyoshi@kddi.com>
2023-08-03 11:29:53 +0900

    terraform デモ

--- backend/terraform/s3.tf
+++ backend/terraform/s3.tf
@@ -7,48 +7,48 @@ resource "aws_s3_bucket_public_access_block" "example" {
 
   block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_object" "object" {
-  bucket = aws_s3_bucket.sample.id
-  key = "index.html"
-  source = "./src/index.html"
-  etag = filemd5("./src/index.html")
-  content_type = "text/html; charset=utf-8"
-  lifecycle {
-    ignore_changes = [
-      etag
-    ]
-  }
-}
+# resource "aws_s3_bucket_object" "object" {
+#   bucket = aws_s3_bucket.sample.id
+#   key = "index.html"
+#   source = "./src/index.html"
+#   etag = filemd5("./src/index.html")
+#   content_type = "text/html; charset=utf-8"
+#   lifecycle {
+#     ignore_changes = [
+#       etag
+#     ]
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "policy" {
-  bucket = aws_s3_bucket.sample.id
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Test",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": [
-        "s3:GetObject"
-      ],
-      "Resource": [
-        "arn:aws:s3:::aufgiesser-sample-bucket-${var.env}/*"
-      ]
-    }
-  ]
-}
-POLICY
-}
+# resource "aws_s3_bucket_policy" "policy" {
+#   bucket = aws_s3_bucket.sample.id
+#   policy = <<POLICY
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "Test",
+#       "Effect": "Allow",
+#       "Principal": "*",
+#       "Action": [
+#         "s3:GetObject"
+#       ],
+#       "Resource": [
+#         "arn:aws:s3:::aufgiesser-sample-bucket-${var.env}/*"
+#       ]
+#     }
+#   ]
+# }
+# POLICY
+# }
 
-resource "aws_s3_bucket_website_configuration" "hosting" {
-  bucket = aws_s3_bucket.sample.id
-  index_document {
-    suffix = "index.html"
-  }
-}
+# resource "aws_s3_bucket_website_configuration" "hosting" {
+#   bucket = aws_s3_bucket.sample.id
+#   index_document {
+#     suffix = "index.html"
+#   }
+# }
